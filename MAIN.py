c4653cdba7f11ce501db92d871b4a7ec83dcb25c
gooblesGH <hampsvr@gmail.com>
2026-09-07 16:45:04 +0100

    Refactor MAIN.py to include additional app info

--- MAIN.py
+++ MAIN.py
@@ -1,9 +1,11 @@
+# Plugins
 import sys
 import psutil
 import time
 import getpass
 import os
 
+# Colors
 os.system('color') 
 
 RED = '\033[31m'
@@ -20,12 +22,17 @@ BLUE = '\033[34m'
 #---------------------------------
 print(GREEN + "Welcome! " + getpass.getuser() + RESET)
 time.sleep(1)
+
 print(RED + "Running general tests:\n-----------------------------" + RESET)
 print(RED + "Current System: " + sys.platform + RESET)
+print(RED + "Current CPU Usage: " + str(psutil.cpu_percent()) + "%" + RESET)
 print(RED + "Current Python Version: " + sys.version + RESET)
+
 time.sleep(1)
 
-MALWAREBYTES_ICON = """"                       
+# Icons
+
+MALWAREBYTES_ICON = """                     
        &&                  &&       
      &&&&&                &&&&&     
     &&&&&&&&            &&&&&&&&    
@@ -52,11 +59,33 @@ PROTON_VPN_ICON = """
   ╚═══╝  ╚═╝     ╚═╝  ╚═══╝
   """
 
+TRANSLUCENT_TB_ICON = """
+████████╗██████╗ 
+╚══██╔══╝██╔══██╗
+   ██║   ██████╔╝
+   ██║   ██╔══██╗
+   ██║   ██████╔╝
+   ╚═╝   ╚═════╝ 
+   """
+
+# Initialization message
 print(YELLOW + "-----------------\nSuccussfully loaded\n------------------" + "\n Welcome to recommended PC Apps! This program was created to show\n users, like you! " + getpass.getuser() + " The best apps for their PC!" + RESET)
-time.sleep(4)
+time.sleep(2)
+
+# Licensing and credits
 print(YELLOW + "Information such as licensing and credits are located at: https://github.com/gooblesGH/Recommened-Apps\n-------------------" + RESET)
 time.sleep(2)
+
+# App 1: Malwarebytes
 print(BLUE + "Application #1: Malwarebytes\n"+ "https://www.malwarebytes.com/" + MALWAREBYTES_ICON + "" + RESET)
 time.sleep(1)
+
+# App 2: Proton VPN
 print(BLUE + "--------------------\n Application #2: Proton VPN\nhttps://protonvpn.com/\n" + PROTON_VPN_ICON + "" + RESET)
-input(RED + "------------\nPress Enter to Abort" + RESET)
+
+# App 3: Translucent TB
+print(BLUE + "--------------------\n Application #3: Translucent TB\nhttps://github.com/TranslucentTB/TranslucentTB\n" + TRANSLUCENT_TB_ICON + "" + RESET)
+
+# Closing message
+input(RED + "------------\nPress Enter to Close..." + RESET)
+
